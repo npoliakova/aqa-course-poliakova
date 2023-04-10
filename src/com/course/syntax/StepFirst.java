f7fb5227e98afbc31de3049ebcdf948ee926e59b
npoliakova <poliakova.nataliia@airslate.com>
2023-04-10 14:52:17 +0300

    added first step

---
+++ src/com/course/syntax/StepFirst.java
@@ -0,0 +1,19 @@
+package com.course.syntax;
+
+public class StepFirst {
+    public static void main(String[] args) {
+        byte myByte = 127;
+        short myShort = -32768;
+        int myInt = 2147483647;
+        long myLong = 922337203685477580L;
+        float myFloat = 12.7F;
+        double myDouble = 12.5D;
+        char myStar = '*';
+        boolean myBool = true;
+
+        System.out.println("myByte = " + myByte + " myShort = " + myShort
+                + " myInt = " + myInt + " myLong = " + myLong
+                + " myFloat = " + myFloat + " myDouble = " + myDouble
+                + " myStar = " + myStar + " myBool = " + myBool);
+    }
+}
